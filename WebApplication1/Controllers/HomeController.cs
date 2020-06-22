14705dd41adcb3a1937ea7850c8a16d904fdf22d
hkudumu@outlook.com <hkudumu@outlook.com>
2020-06-23 00:51:52 +0530

    First Chage

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace WebApplication1.Controllers
         public ActionResult Index()
         {
             return View();
-            //master dfjsfbhfbhfjh
+            //master branch
         }
 
         public ActionResult About()
